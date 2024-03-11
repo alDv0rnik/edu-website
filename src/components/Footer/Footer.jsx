32eb024005bb35b4189d65b2bad3bb1c6a116a0b
Aliaksandr Dvornik <aadvornik@gmail.com>
2024-03-12 00:05:41 +0300

    Updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,115 +1,43 @@
- import React from 'react';
- import { Container, Row, Col,  ListGroup, ListGroupItem} from 'reactstrap';
- import './footer.css';
- import footerImg from '../../assets/images/footer_img.jpg';
+import React from 'react';
+import {Container, Row, Col, ListGroup, ListGroupItem} from 'reactstrap';
+import './footer.css';
 
- const footerQuickLinks = [
-        {
-            display : 'Resource',
-            url: '#'
-        },
-        {
-            display : 'Blog',
-            url: '#'
-        },
-        {
-            display : 'Documents',
-            url: '#'
-        },
-        
- ]  
+const footerQuickLinks = [
+    {
+        display: 'На главную',
+        url: 'http://localhost:5173/'
+    },
+    {
+        display: 'Материалы',
+        url: '#'
+    },
+    {
+        display: 'Тесты',
+        url: '#'
+    },
+]
 
-
-  const footerComLinks = [
-       
-        {
-            display : 'Support',
-            url: '#'
-        },
-        {
-            display : 'Help',
-            url: '#'
-        },
-        
- ] 
-
- const footerInfoLinks = [
-        {
-            display : 'About Us',
-            url: '#'
-        },
-        {
-            display : 'Partners',
-            url: '#'
-        },
-        {
-            display : 'Contact us',
-            url: '#'
-        },
-        
- ] 
- 
-  const footerCourseskLinks = [
-        {
-            display : 'Python',
-            url: '#'
-        },
-        {
-            display : 'C',
-            url: '#'
-        },
-        {
-            display : 'C++',
-            url: '#'
-        },
-         {
-            display : 'JavaScript',
-            url: '#'
-        },
-         {
-            display : 'Java',
-            url: '#'
-        },
-         {
-            display : 'Rust',
-            url: '#'
-        },
-         {
-            display : 'Golang',
-            url: '#'
-        },
-         {
-            display : 'Kotlin',
-            url: '#'
-        },
-         {
-            display : 'Swift',
-            url: '#'
-        },
-         {
-            display : 'R',
-            url: '#'
-        },
-         {
-            display : 'SQL',
-            url: '#'
-        },
-        
- ] 
- 
+const footerInfoLinks = [
+    {
+        display: 'О нас',
+        url: '#'
+    },
+    {
+        display: 'Контакты',
+        url: '#'
+    },
+]
 
 
- const Footer = () => {
+const Footer = () => {
     return <footer className="footer">
         <Container>
             <Row>
-                  
-
                 <Col lg='3' md='6'>
-                    <h6 className='text-white pt-4'>Courses</h6>
+                    <h6 className='text-white pt-4'>Навигация</h6>
                     <ListGroup className='link_list'>
                         {
-                            footerCourseskLinks.map((item,index)=> (
+                            footerQuickLinks.map((item, index) => (
                                 <ListGroupItem key={index} className='border-0 ps-0 link_item'><a href={item.url}>
                                     {item.display}
                                 </a>
@@ -119,12 +47,11 @@
                         }
                     </ListGroup>
                 </Col>
-
                 <Col lg='3' md='6'>
-                    <h6 className='text-white pt-4'>Explore</h6>
+                    <h6 className='text-white pt-4'>МГПУ</h6>
                     <ListGroup className='link_list'>
                         {
-                            footerQuickLinks.map((item,index)=> (
+                            footerInfoLinks.map((item, index) => (
                                 <ListGroupItem key={index} className='border-0 ps-0 link_item'><a href={item.url}>
                                     {item.display}
                                 </a>
@@ -134,80 +61,15 @@
                         }
                     </ListGroup>
                 </Col>
-
-                <Col lg='3' md='6'>
-                    <h6 className='text-white pt-4'>Community</h6>
-                    <ListGroup className='link_list'>
-                        {
-                            footerComLinks.map((item,index)=> (
-                                <ListGroupItem key={index} className='border-0 ps-0 link_item'><a href={item.url}>
-                                    {item.display}
-                                </a>
-
-                                </ListGroupItem>
-                            ))
-                        }
-                    </ListGroup>
-                </Col>
-
-                <Col lg='3' md='6'>
-                    <h6 className='text-white pt-4'>Rapt 'N' Rel</h6>
-                    <ListGroup className='link_list'>
-                        {
-                            footerInfoLinks.map((item,index)=> (
-                                <ListGroupItem key={index} className='border-0 ps-0 link_item'><a href={item.url}>
-                                    {item.display}
-                                </a>
-
-                                </ListGroupItem>
-                            ))
-                        }
-                    </ListGroup>
-                </Col>
-
                 <Col lg='12' className=''>
-                    <div className='d-flex align-items-center justify-content-center gap-2 p-4 logo'>
-                        <img src={footerImg} alt="" />
-                    </div>
-
                     <div className='follows'>
-                        <p className='mb-0 d-flex align-items-center justify-content-center gap-2 text-white'>© Nuevicsu Tech. All rights reserved.</p> 
-                        
-                        <div className='d-flex align-items-center justify-content-center gap-2 links_socials'>
-                            <span>
-                            {" "}
-                            <a href="facebook.com">
-                                <i class="ri-facebook-fill text-white"></i>
-                            </a>
-                        </span>
-
-                        <span>
-                            {" "}
-                            <a href="twitter.com">
-                                <i class="ri-twitter-fill text-white"></i>
-                            </a>
-                        </span>
-
-                        <span>
-                            {" "}
-                            <a href="instagram.com">
-                                <i class="ri-instagram-line text-white"></i>
-                            </a>
-                        </span>
-
-                        <span>
-                            {" "}
-                            <a href="linkedln.com">
-                                <i class="ri-linkedin-fill text-white"></i>
-                            </a>
-                        </span>
-                        </div>
-
+                        <p className='mb-0 d-flex align-items-center justify-content-center gap-2 text-white'>©МГПУ. Все
+                            права защищены</p>
                     </div>
-                </Col> 
+                </Col>
             </Row>
         </Container>
     </footer>
- };
+};
 
- export default Footer ;
+export default Footer;
